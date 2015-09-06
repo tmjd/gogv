8e8872c2fb2015401723d789beef5e735d347e34
tomjoad <tomjoad@pushmepullme.net>
2015-09-05 23:22:39 -0500

    Removed all the debugging output that was on by default. Added a state variable to keep track of the logged in state and bail out of calls that need to be logged in to do.

--- gv.go
+++ gv.go
@@ -14,19 +14,17 @@ import (
 var ErrLoginFailed = errors.New("Failed to login")
 var ErrLogoutFailed = errors.New("Failed to logout")
 var ErrSendSmsFailed = errors.New("Failed to send sms")
+var ErrNotLoggedIn = errors.New("Not logged in")
 
 type GV struct {
-	login_email string
-	password    string
-	page_map    map[string]string
 	http_client http.Client
 	rnr_se      string //special code for connections?
+	ShowStatus  bool
+	logged_in   bool
 }
 
-func (g *GV) Login(email string, passwd string) error {
-	g.login_email = email
-	g.password = passwd
-
+func (g *GV) Login(email string, password string) error {
+	g.logged_in = false
 	options := cookiejar.Options{
 		PublicSuffixList: publicsuffix.List,
 	}
@@ -43,32 +41,28 @@ func (g *GV) Login(email string, passwd string) error {
 		return ErrLoginFailed
 	}
 
-	fmt.Println(resp.Status)
+	if g.ShowStatus {
+		fmt.Println("Primer request " + resp.Status)
+	}
 	body, err := httputil.DumpResponse(resp, true)
 	resp.Body.Close()
 	if err != nil {
 		return ErrLoginFailed
 	}
-	//fmt.Println(string(body[:]))
-	galx := string(regexp.MustCompile(
-		`name="GALX" type="hidden"\s*value="(.+)"`).FindSubmatch(body)[1])
-	service := string(regexp.MustCompile(
-		`name="service" type="hidden"\s*value="(.+)"`).FindSubmatch(body)[1])
-	utf8 := string(regexp.MustCompile(
-		`type="hidden" .* name="_utf8" *value="(.+)"`).FindSubmatch(body)[1])
-	bgresponse := string(regexp.MustCompile(
-		`type="hidden" name="bgresponse" .* *value="(.+)"`).FindSubmatch(body)[1])
+	re := regexp.MustCompile(`name="GALX" .*\s*value="(.+)"`)
+	galx := string(re.FindSubmatch(body)[1])
+	re = regexp.MustCompile(`name="service" .*\s*value="(.+)"`)
+	service := string(re.FindSubmatch(body)[1])
+	re = regexp.MustCompile(`name="_utf8" *value="(.+)"`)
+	utf8 := string(re.FindSubmatch(body)[1])
+	re = regexp.MustCompile(`name="bgresponse" .* *value="(.+)"`)
+	bgresponse := string(re.FindSubmatch(body)[1])
 
 	pstMsg := "1"
 
-	fmt.Println("Galx " + string(galx))
-	fmt.Println("Service " + string(service))
-	fmt.Println("Utf8 " + string(utf8))
-	fmt.Println("bgresponse " + string(bgresponse))
-
 	resp, err = g.http_client.PostForm(
 		"https://accounts.google.com/ServiceLogin?service=grandcentral",
-		url.Values{"Email": {g.login_email}, "Passwd": {g.password},
+		url.Values{"Email": {email}, "Passwd": {password},
 			"GALX": {galx}, "_utf8": {utf8},
 			"bgresponse": {bgresponse}, "pstMsg": {pstMsg},
 			"service":  {service},
@@ -78,40 +72,52 @@ func (g *GV) Login(email string, passwd string) error {
 		return ErrLoginFailed
 	}
 
-	fmt.Println(resp.Status)
+	if g.ShowStatus {
+		fmt.Println("login respone: " + resp.Status)
+	}
 	body, err = httputil.DumpResponse(resp, true)
 	resp.Body.Close()
 	if err != nil {
 		return ErrLoginFailed
 	}
 
-	g.rnr_se = string(regexp.MustCompile(
-		"'_rnr_se': '(.+)'").FindSubmatch(body)[1])
-	//fmt.Println(string(body[:]))
-	fmt.Println("rnr_se " + g.rnr_se)
+	re = regexp.MustCompile("'_rnr_se': '(.+)'")
+	g.rnr_se = string(re.FindSubmatch(body)[1])
 
-	return ErrLoginFailed
+	if len(g.rnr_se) == 0 {
+		return ErrLoginFailed
+	}
+	g.logged_in = true
+
+	return nil
 }
 
 func (g *GV) SendSms(phoneNumber string, text string) error {
+	if g.logged_in == false {
+		return ErrNotLoggedIn
+	}
 
 	resp, err := g.http_client.PostForm(
 		"https://www.google.com/voice/sms/send",
 		url.Values{"phoneNumber": {phoneNumber}, "text": {text},
 			"_rnr_se": {g.rnr_se}})
 	defer resp.Body.Close()
-	fmt.Println("Send SMS response " + resp.Status)
-	body, err := httputil.DumpResponse(resp, true)
+	if g.ShowStatus {
+		fmt.Println("Send SMS response " + resp.Status)
+	}
+	_, err = httputil.DumpResponse(resp, true)
 	if err != nil {
 		return ErrSendSmsFailed
 	}
-
-	fmt.Println(string(body[:]))
 
 	return nil
 }
 
 func (g *GV) Logout() error {
+	if g.logged_in == false {
+		return nil
+	}
+	g.logged_in = false
 	resp, err := g.http_client.Get(
 		"https://www.google.com/voice/account/signout",
 	)
@@ -120,11 +126,12 @@ func (g *GV) Logout() error {
 	}
 
 	defer resp.Body.Close()
-	fmt.Println("Send SMS response " + resp.Status)
-	body, err := httputil.DumpResponse(resp, true)
+	if g.ShowStatus {
+		fmt.Println("Logout response status " + resp.Status)
+	}
+	_, err = httputil.DumpResponse(resp, true)
 	if err != nil {
 		return ErrSendSmsFailed
 	}
-	fmt.Println(string(body[:]))
 	return nil
 }
